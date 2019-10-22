6ada1a867d870bf3f1d2219370917d4af19aac60
swarnaacharya <55823914+swarnaacharya@users.noreply.github.com>
2019-10-22 18:07:50 +0530

    hibernate-demo

---
+++ hibernate-demo/src/main/java/com/mycompany/hibernate_demo/App.java
@@ -0,0 +1,40 @@
+package com.mycompany.hibernate_demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mycompany.hibernate_demo.entity.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       try {
+    	   
+    	   SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").
+    			   addAnnotatedClass(Student.class).buildSessionFactory();
+    	   Session session=factory.openSession();
+    	   Student student=new Student();
+    	   student.setFirstName("John");
+    	   student.setLastName("Doe");
+    	   student.setEmail("john@luv2code.com");
+    	   session.getTransaction().begin();
+    	   session.save(student);
+    	   session.getTransaction().commit();
+    	   System.out.println("sucess");
+    	   session.close();
+  
+    	   
+    	   
+    	   
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+    }
+}
